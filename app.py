d60728da9bdf508ad8af1485a0959ea2c774bd0f
Maximiliano Merino Rios <maximiliano.merino@integratel.com.pe>
2026-06-15 17:14:20 -0500

    prueba error backend 15-06

--- app.py
+++ app.py
@@ -1322,7 +1322,7 @@ def guardar_historico_endpoint():
     params = _get_params()
 
     fecha_inicio = _parse_date(
-        params.get("inicio", "2025-11-01"), "inicio"
+        params.get("inicio", "2025-12-01"), "inicio"
     )
     fecha_fin = _parse_date(
         params.get("fin", _ayer_lima_str()), "fin"   # Por defecto: ayer en hora Lima
